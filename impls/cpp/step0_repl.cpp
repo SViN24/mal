73e7f2f9d53f5111e432b7f4e4531703477df1c7
John Iliopoulos <svin@dismail.de>
2021-04-17 19:29:57 +0300

    added command line editing

--- impls/cpp/step0_repl.cpp
+++ impls/cpp/step0_repl.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+#include "linenoise.hpp"
+
 std::string READ(std::string input){ return input; }
 
 std::string EVAL(std::string input){ return input; }
@@ -17,14 +19,24 @@ std::string rep(std::string input)
 
 int main()
 {
+
+    const auto histpath = "malhist";
+
+    linenoise::SetHistoryMaxLen(4);
+    linenoise::LoadHistory(histpath);
+
     std::string input;
 
     for(;;)
     {
-        std::cout << "user> ";
-        if(!std::getline(std::cin, input))
+
+        auto quit = linenoise::Readline("user> ", input);
+        if (quit)
             break;
+
+        linenoise::AddHistory(input.c_str());
         std::cout << rep(input) << std::endl;
     }
+    linenoise::SaveHistory(histpath);
     return 0; 
 }
