656708f6c413c8f3ab620cbc6f0c6c763fe41f1c
SViN24 <svin@dismail.de>
2021-04-17 15:45:42 +0300

    new CPP implementation

---
+++ impls/cpp/step0_repl.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+std::string READ(std::string input){ return input; }
+
+std::string EVAL(std::string input){ return input; }
+
+std::string PRINT(std::string input){ return input; }
+
+std::string rep(std::string input)
+{ 
+    auto readline = READ(input);
+    auto result = EVAL(readline);
+    return PRINT(result);
+}
+
+
+int main()
+{
+    std::string input;
+
+    for(;;)
+    {
+        std::cout << "user> ";
+        if(!std::getline(std::cin, input))
+            break;
+        std::cout << rep(input) << std::endl;
+    }
+    return 0; 
+}
